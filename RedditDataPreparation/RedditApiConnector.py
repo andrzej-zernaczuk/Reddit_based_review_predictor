e4b93ec03b9d05a35ab5c7acdc5ff2aab62af7e7
Jakub Wujec <jakub_wujec@perfect-data.pl>
2021-05-08 01:08:47 +0200

    submissions

--- RedditDataPreparation/RedditApiConnector.py
+++ RedditDataPreparation/RedditApiConnector.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from psaw import PushshiftAPI
+import numpy as np
 
 class RedditApiConnector:
 
@@ -15,11 +16,28 @@ class RedditApiConnector:
         filter = ['author', 'date', 'title', 'body', 'score']
 
         # Connect to api, and get data
-        comments = self.api.search_comments(kwargs,
-            subreddit = self.subreddit, filter=filter,
-            q=self.name, limit=limit
+        comments = self.api.search_comments(
+            kwargs,subreddit = self.subreddit,
+            filter=filter, q=self.name, limit=limit
         )
         # Prepare dataframe
         df = pd.DataFrame([comment.d_ for comment in comments])
+
+        return df
+
+    def search_submissions(self, limit=1000, **kwargs):
+        # Hardcode filters to keep same columns in dataframes
+        filer = ['author', 'date', 'title', 'selftext', 'score']
+
+        submissions = self.api.search_submissions(
+            kwargs, subreddit=self.subreddit,
+            filer=filer, q=self.name, limit=limit
+        )
+
+        df = pd.DataFrame([submission.d_ for submission in submissions])
+        # Rename columns to keep all dataframes same
+        df.rename(columns={'selftext': 'body'}, inplace=True)
+        # Replace '[removed]' with NaN values
+        df['body'].replace(['removed'], np.NaN, inplace=True)
 
         return df
