69af70b20be604802a6ff85c9207893376e92e07
kuba1302 <kuba1302@gmail.com>
2021-05-16 21:23:33 +0200

    added few files

---
+++ Models/FirstCNN.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+
+class FirstCNN:
+
+    def __init__(self):
+        self.model = tf.keras.models.load_model('../first_model')
+        """
+        Model architecture:
+        model = Sequential(
+            [
+            tf.keras.layers.Conv1D(filters=32, 
+                kernel_size=10,
+                strides=10,
+                padding='valid',
+                activation='relu'),
+                tf.keras.layers.MaxPool1D(pool_size=2),
+                tf.keras.layers.Flatten(),
+                tf.keras.layers.Dense(10, activation='sigmoid'),
+                tf.keras.layers.Dense(1, activation='sigmoid')
+            ]
+        )
+        Layer (type)                 Output Shape              Param #   
+        =================================================================
+        embedding_3 (Embedding)      (None, 100, 32)           320000    
+        _________________________________________________________________
+        conv1d_4 (Conv1D)            (None, 10, 32)            10272     
+        _________________________________________________________________
+        max_pooling1d_3 (MaxPooling1 (None, 5, 32)             0         
+        _________________________________________________________________
+        flatten_3 (Flatten)          (None, 160)               0         
+        _________________________________________________________________
+        dense_1 (Dense)              (None, 10)                1610      
+        _________________________________________________________________
+        dense_2 (Dense)              (None, 1)                 11        
+        =================================================================
+        Total params: 331,893
+        Trainable params: 331,893
+        Non-trainable params: 0
+        """
+
+    def predict_sentiment(self, tf_df):
+        predictions = self.model.predict(tf_df)
+        mean = predictions.mean()
+
+        return predictions, mean
+
---
+++ RedditDataPreparation/DataPreprocessing.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+import pickle
+
+class DataPreprocesser:
+
+    def __init__(self, data, text_col):
+
+        self.data = data
+        self.text_col = text_col
+        nltk.download('stopwords')
+
+    @staticmethod
+    def initial_data_prep(text):
+        text = text.str.lower()
+        # Replace new rows with space
+        text = text.replace('\n', " ").replace("\r", " ")
+        # Create list of all not needed chars
+        punc_list = '!"@#$%^&*()+_-.<>?/:;[]{}|\~'
+        # Make transformation with dict that contains punc_list chars
+        t = str.maketrans(dict.fromkeys(punc_list, " "))
+        # Apply transformation
+        text = text.translate(t)
+        # Replace single quote with empty char
+        t = str.maketrans(dict.fromkeys("'`", ""))
+        text = text.translate(t)
+
+        return text
+
+    @staticmethod
+    def remove_stop_words(text):
+        # Prepare set of stopwords
+        stop_words = set(stopwords.words('english'))
+        # Remove stopwords from the text
+        filtered_text = [word for word in text.split() if not word in stop_words]
+
+        return filtered_text
+
+    @staticmethod
+    def text_stemming(text):
+        stemmer = nltk.porter.PorterStemmer()
+        text = [stemmer.stem(word) for word in text]
+        return text
+
+    def clean_data(self, data, text_col):
+        data = data.copy()
+        data[text_col] = data[text_col].apply(lambda x: self.initial_data_prep(x)).copy()
+        # Apply func that removes stop words
+        data[text_col] = data[text_col].apply(lambda x: self.remove_stop_words(x)).copy()
+        # Apply func that performs lemmatization
+        data[text_col] = data[text_col].apply(lambda x: self.text_stemming(x))
+
+        return data
+
+
+    def tokenize(self, data, text_col, num_words_pad):
+        # Initialize tokenizer
+        with open('Data/tokenizer.pickle', 'rb') as handle:
+            tok = pickle.load(handle)
+        seq = tok.texts_to_sequences(list(data[text_col]))
+        # Pad sequences to make them same lenght
+        tf_ready = tf.keras.preprocessing.sequence.pad_sequences(seq, maxlen=num_words_pad)
+        # Prepare dataframe ready for training
+        tf_df = pd.DataFrame(tf_ready)
+
+        return tf_df
+
