1335d9b1b8ac39b3ed496cf031111d04c275da00
kuba1302 <63107459+kuba1302@users.noreply.github.com>
2021-06-17 22:03:41 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,23 +8,23 @@ import time
 
 class RedditBasedPredictor:
 
-    def __init__(self, name, subreddit='movies'):
-        self.name = name
+    def __init__(self, subreddit='movies'):
         self.subreddit = subreddit
         self.model = FirstCNN()
-        self.connector = RedditApiConnector(self.name, self.subreddit)
+        self.connector = RedditApiConnector(self.subreddit)
         self.data = self.get_data()
         self.preprocessor = DataPreprocesser(self.data, 'body')
 
-    def get_data(self):
-        return self.connector.search_comments()
+    def get_data(self, name):
+        return self.connector.search_comments(name)
 
-    def make_prediction(self):
-        clean_data = self.preprocessor.full_prepare_data(self.get_data(), 'body')
+    def make_prediction(self, name):
+        clean_data = self.preprocessor.full_prepare_data(self.get_data(name), 'body')
         return self.model.predict_sentiment(clean_data)
 
     # Pomyslec nad wagami takimi zeby wywalalo te z ujemnym scorem
-    def prepare_avg(self, preds):
+    def prepare_avg(self, name):
+        preds = self.make_prediction(name)
         weights_reshape = np.reshape(self.data['score'].values, (preds.shape))
         return np.average(preds, weights=weights_reshape)
 
@@ -36,13 +36,9 @@ class ErrorMeasuring:
         df = pd.read_json('Filmweb_top50.json')
 
 
-# XDDDD TE WYNIKI TAKIE NIE ZA DOBRE
-# ZMIENIC DLUGOSC TENSORA PO TOKENIZACJI BO W TRENOWANYM DATASECIE BYLY PONAD 100 A TUTAJ SREDNIA TO 48
-# OGRNAC Z POSTOW A NIE KOMENTARZY TYLKO
-# Z 40 COS NIE DZIALA, MOZE PRZY ZA DLUGICH TRZEBA ZOBACZYC
+
 if __name__ == '__main__':
     start = time.time()
-    reddit = RedditBasedPredictor('Joker')
-    preds = reddit.make_prediction()
-    print(reddit.prepare_avg(preds))
+    reddit = RedditBasedPredictor()
+    print(reddit.prepare_avg('Joker'))
     print(f'---------TIME: {time.time() - start}---------')
